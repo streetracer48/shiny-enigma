1d9b2686c0d95a9b06d84a876c377ebff613e1ca
karim <karim@csit.fi>
2020-04-15 18:04:40 +0300

    search section created

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,19 @@
 import React, { Fragment, Component } from 'react';
 import { BrowserRouter as Router , Route , Switch } from 'react-router-dom';
 import Navbar from './layouts/Navbar'
+import SearchArea from './layouts/SearchArea'
 import './App.css';
 
 
 class App extends Component {
   render()
    {
-
     return (
       <div className="App">
-     
-
       <Router>
         <Fragment>
           <Navbar />
+          <Route path="/" exact component={ SearchArea }/>
         </Fragment>
       </Router>
      
